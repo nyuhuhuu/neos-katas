b279e20865b249a1eea2a500af378a4289776f5f
Gabor Torok <gtorok@babbel.com>
2018-06-17 21:31:47 +0200

    Add test cases and solution for async retail calculator
    It fetches discount rates from backend service and uses the response to
    calculate total price.

--- retail-calc-async/index.js
+++ retail-calc-async/index.js
@@ -1,7 +1,30 @@
 const axios = require('axios');
 const config = require('./config');
 
-function AsyncRetailCalculator() {
+function AsyncRetailCalculator(quantity, unitPrice) {
+    if (arguments.length != 2) {
+        throw new Error("AsyncRetailCalculator() takes two arguments: quantity and unit price");
+    }
+    this.quantity = quantity;
+    this.unitPrice = unitPrice;
 }
+
+AsyncRetailCalculator.prototype.getTotal = function() {
+    let total = this.quantity * this.unitPrice;
+    return new Promise(function(resolve, reject) {
+        axios.get(config.serverUrl + '/discounts/')
+            .then(function(response) {
+                const discounts = response.data;
+                let rate = discounts.reverse().find((discount) => discount[0] < total);
+                if (typeof rate != 'undefined') {
+                    total = total * (1 - rate[1]);
+                }
+                resolve(total);
+            })
+            .catch(function(error) {
+                reject(error);
+            });
+    });
+};
 
 module.exports = AsyncRetailCalculator;
--- retail-calc-async/test.js
+++ retail-calc-async/test.js
@@ -8,8 +8,34 @@ chai.should();
 
 describe('Calculates total price with discount', function() {
 
-    it('Takes 2 inputs', function() {
+    it('Takes 2 input', function() {
         assert.throws(() => { return new Calc(); }, Error);
+        assert.throws(() => { return new Calc(5); }, Error);
+        assert.throws(() => { return new Calc(5, 1.2, 'NV'); }, Error);
+        assert.doesNotThrow(() => { return new Calc(5, 1.2); }, Error);
     });
 
+    it('Calculates total with no discount', function(done) {
+        (new Calc(5, 1.2)).getTotal().should.eventually.equal(6).notify(done);
+    });
+
+    it('Calculates total with 3% discount', function(done) {
+        (new Calc(50, 20.5)).getTotal().should.eventually.equal(994.25).notify(done);
+    });
+
+    it('Calculates total with 5% discount', function(done) {
+        (new Calc(100, 60)).getTotal().should.eventually.equal(5700).notify(done);
+    });
+
+    it('Calculates total with 7% discount', function(done) {
+        (new Calc(120, 60)).getTotal().should.eventually.equal(6696).notify(done);
+    });
+
+    it('Calculates total with 10% discount', function(done) {
+        (new Calc(2000, 14.5)).getTotal().should.eventually.equal(26100).notify(done);
+    });
+
+    it('Calculates total with 15% discount', function(done) {
+        (new Calc(45, 1500)).getTotal().should.eventually.equal(57375).notify(done);
+    });
 });
