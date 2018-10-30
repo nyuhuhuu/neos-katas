ba59c33e9e2b55cb377121c99c6579870e085af2
Gabor Torok <gtorok@babbel.com>
2018-10-30 13:29:48 +0100

    Make kata independent from the browser

---
+++ simple-decorator/kata-1.js
@@ -0,0 +1,37 @@
+const loginUser = (state) => {
+  state.userId = "54fs-/REex";
+  console.log("successful login");
+};
+
+const listFavFruits = (state, db) => console.log("words:", db[state.userId]);
+
+const isUserLoggedIn = (state) => state.userId != null;
+
+const logoutUser = (state) => {
+  if (!isUserLoggedIn(state)) throw new Error("User is not logged in!");
+  state.userId = null;
+  console.log("successful logout");
+};
+
+(() => {
+  const state = { userId: null };
+  const db = { "54fs-/REex": ["ananas", "mango", "peach"] };
+
+  console.log("\n----- Returns undefined if user was not explicitly logged in -----");
+  listFavFruits(state, db); // undefined
+  console.log("\n----- Works because user was already logged in -----");
+  loginUser(state);
+  listFavFruits(state, db); // fruits
+  logoutUser(state);
+
+  // TODO Write a function `loginRequired()` that wraps its function argument.
+  // If user was not logged in yet, it calls `loginUser()` first. Then it calls
+  // its function argument.
+
+  if (typeof loginRequired === 'function') {
+    console.log("\n----- Calling the function will log in the user first -----");
+    const listFavFruitsLoginRequired = loginRequired(listFavFruits);
+    listFavFruitsLoginRequired(state, db);
+  }
+
+})();
